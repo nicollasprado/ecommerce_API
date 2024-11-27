d7e22301f7ce2809995f4e8d92276cbc986fbdca
nicollasprado <nicollasprado.profissional@gmail.com>
2024-11-27 18:38:10 -0300

    Projeto SpringBoot

---
+++ src/test/java/com/nicollasprado/gerenciadorPedidos/GerenciadorPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicollasprado.gerenciadorPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
